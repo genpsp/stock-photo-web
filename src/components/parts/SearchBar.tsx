4e7d8137dcccc7f92c7cf85c588bcf2fff488254
梅垣玄 <genpsp10@gmail.com>
2024-01-15 01:08:26 +0900

    検索バー作成

---
+++ src/components/parts/SearchBar.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import Paper from '@mui/material/Paper'
+import InputBase from '@mui/material/InputBase'
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import SearchIcon from '@mui/icons-material/Search'
+import DirectionsIcon from '@mui/icons-material/Directions'
+
+export default function SearchBar() {
+  return (
+    <Paper
+      component="form"
+      sx={{
+        p: '0px 8px',
+        display: 'flex',
+        alignItems: 'center',
+        borderRadius: '25px',
+      }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="画像を検索"
+        inputProps={{ 'aria-label': 'search iamges' }}
+      />
+      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  )
+}
