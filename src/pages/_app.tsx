5bd7fa12af3245796375fe65d6e92800e8d90ea7
g.umegaki <g.umegaki@buysell-technologies.com>
2024-01-16 18:13:55 +0900

    firebase導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
+import { app } from '@/lib/firebase/init'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const firebaseApp = app
   return <Component {...pageProps} />
 }
